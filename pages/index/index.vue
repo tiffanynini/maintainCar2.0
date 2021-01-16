98cc57c7d5b135bd81e4d1848aa92444beeb4f9e
tiffany <1326593858@qq.com>
2021-01-16 15:58:58 +0800

    项目结构搭建

---
+++ pages/index/index.vue
@@ -0,0 +1,37 @@
+<template>
+	<div>
+		<uni-search-bar></uni-search-bar>
+		
+	</div>
+	
+</template>
+
+<script>
+	
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="less">
+	@import url("../../static/font/iconfont.css");
+	div{
+		width: 100%;
+		height: 100rpx;
+		uni-search-bar{
+			width: 100rpx;
+			height: 100rpx;
+		}
+	}
+</style>
