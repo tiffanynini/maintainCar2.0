97af22d0da01d0000d1a6f77acdcf5b2172d3f97
Hola <1264590756@qq.com>
2021-01-18 18:40:09 +0800

    发现二级页面完成

--- pages/findInfo/findInfo.vue
+++ pages/findInfo/findInfo.vue
@@ -6,6 +6,32 @@
 		<view class="titleInfo">
 			发表于2020-10-20 20:17:36  阅读:1238
 		</view>
+		<view class="video">
+			<video src="http://player.youku.com/embed/XMzAzMjE2NTk4MA" controls style="width: 100%;"></video>
+		</view>
+		<view class="comment">
+			评论
+		</view>
+		<view class="content" :key="i" v-for="(v,i) in arr">
+			<view class="left">
+				<image src="http://cloud.axureshop.com/gsc/9VEHLV/09/35/c2/0935c276df9445ff87848efc94e49e75/images/%E6%B1%BD%E8%BD%A6%E4%BF%9D%E5%85%BB%E5%85%AD%E5%AE%97%E7%BD%AA/u505.svg?token=869c853d04e98132a61297fb381b7afc5e636bce4d88113c8d1f548de433b6b5" style="width: 50rpx;" mode="widthFix"></image>
+			</view>
+			<view class="right">
+				<view class="right-top">
+					<view class="right-top-left">
+						<view>无惧</view>
+						<view>2019-07-02</view>
+					</view>
+					<view class="right-top-right">
+						<view>5466</view>
+						<view class="iconfont icon-dianzan"></view>
+					</view>
+				</view>
+				<view class="right-bottom">
+					说得太棒了，get...以后再不用担心胎压过高或者过低的情况了
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -13,7 +39,7 @@
 	export default {
 		data() {
 			return {
-				
+				arr:["","","",""]
 			};
 		},
 		created() {
@@ -25,16 +51,61 @@
 </script>
 
 <style lang="less">
+	@import url("@/static/font/iconfont.css");
 	.box{
-		padding: 10rpx;
+		padding: 10rpx 20rpx;
+		.title{
+			font-size: 36rpx;
+			font-weight: bold;
+		}
+		.titleInfo{
+			margin-top: 10rpx;
+			font-size: 24rpx;
+			color: #AAAAAA;
+		}
+		.video{
+			margin-top: 20rpx;
+		}
+		.content{
+			margin-top: 30rpx;
+			padding-bottom: 20rpx;
+			border-bottom: 1px solid #AAAAAA;
+			display: flex;
+			.left{
+				text-align: center;
+				flex: 1;
+			}
+			.right{
+				flex: 5;
+				font-size: 24rpx;
+				display: flex;
+				flex-direction: column;
+				.right-bottom{
+					margin-top: 8rpx;
+					font-size: 28rpx;
+				}
+				.right-top{
+					display: flex;
+					justify-content: space-between;
+					.right-top-right{
+						display: flex;
+						.icon-dianzan{
+							margin-left: 10rpx;
+						}
+					}
+				}
+			}
+		}
+		.content:nth-last-of-type(1){
+			border-bottom: 0;
+		}
+		.comment{
+			padding-bottom: 14rpx;
+			color: #AAAAAA;
+			font-size: 24rpx;
+			margin-top: 50rpx;
+			border-bottom: 1px solid #AAAAAA;
+		}
 	}
-	.title{
-		font-size: 36rpx;
-		font-weight: bold;
-	}
-	.titleInfo{
-		margin-top: 10rpx;
-		font-size: 24rpx;
-		color: #AAAAAA;
-	}
+	
 </style>
